6c76a6eaaa82a05dd4d1d5576e118ea70ade6063
Andrea <andrea.benaglia@cern.ch>
2014-12-17 10:41:07 +0100

    first import

---
+++ scripts/myRoot.sh
@@ -0,0 +1,17 @@
+export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.18/x86_64-slc6-gcc47-opt/root/
+
+if [[ $PATH == *${ROOTSYS}/bin* ]]
+then
+echo "root PATH already defined"
+else
+export PATH=${ROOTSYS}/bin:${PATH}
+fi
+
+if [[ $LD_LIBRARY_PATH == *${ROOTSYS}/lib* ]]
+then
+echo "root LD_LIBRARY_PATH already defined"
+else
+export LD_LIBRARY_PATH=${ROOTSYS}/lib:${LD_LIBRARY_PATH}
+fi
+
+alias root='root -l'
